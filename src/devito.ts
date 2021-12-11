b70f7b4be92aaebe01f1eae9a7c4e94d99256f9b
BrianMiltonJr <harlanbmiltonjr@gmail.com>
2021-12-10 18:42:54 -0600

    Began random roll code

--- src/devito.ts
+++ src/devito.ts
@@ -1,5 +1,6 @@
 import { Client } from 'twitter.js';
 import { bearerToken } from './secrets';
+import { quotes } from './quotes';
 
 const client = new Client({
     events: [ 'FILTERED_TWEET_CREATE' ]
@@ -19,4 +20,24 @@ client.on('filteredTweetCreate', async tweet => {
     await tweet.like();
 });
 
-client.loginWithBearerToken(bearerToken)
+client.loginWithBearerToken(bearerToken);
+
+function RollDevito() {
+    let number = Math.floor(Math.random() * 6);
+    switch (number) {
+        case 0:
+            return '';
+        case 1:
+            return '';
+        case 2:
+            return '';
+        case 3:
+            return '';
+        case 4:
+            return '';
+        case 5:
+            return '';
+        default:
+            return '';
+    }
+}
